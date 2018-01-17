84828ed3f6b4a4a178a3294c973dd7c9887e86b4
Akshat Joshi <debaser990@gmail.com>
2018-01-18 04:18:01 +0530

    Add files via upload

--- ClassAlgos.py
+++ ClassAlgos.py
@@ -1,12 +1,10 @@
 import pandas
-from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -15,24 +13,23 @@ from sklearn.svm import SVC
 
 url = "C:\Users\Akshat\Downloads\iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=names)
+dataset = pandas.read_csv(url, names=names) #laod dataset
 
 print(dataset.shape) # 150 X 5
 
 print(dataset.groupby('class').size())
 
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
+plt.show() #visualizing data
 
-array = dataset.values
+array = dataset.values #splitting dataset into training and testing set
 X = array[:,0:4]
 Y = array[:,4]
 validation_size = 0.20
-seed = 7
+seed = 7 #psuedo random seed
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
-
-seed = 7
-scoring = 'accuracy'
+seed = 7 #reset random seed
+scoring = 'accuracy' # metric used to evaluate diff models  
 
 models = []
 models.append(('LR', LogisticRegression()))
@@ -50,4 +47,29 @@ for name, model in models:
 	results.append(cv_results)
 	names.append(name)
 	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
+	print(msg+"\n")
+
+print("Support Vector Machine:\n")	
+svm= SVC() #most accurate model
+svm.fit(X_train,Y_train)
+predict=svm.predict(X_validation)  
+print(accuracy_score(Y_validation, predict))
+print(confusion_matrix(Y_validation, predict))
+print(classification_report(Y_validation, predict))
+
+print("K Nearest Neighbors :\n")
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+
+print("Logistic Regression :\n")
+lr = LogisticRegression() # least accurate
+lr.fit(X_train, Y_train)
+prediction3 = lr.predict(X_validation)
+print(accuracy_score(Y_validation, prediction3))
+print(confusion_matrix(Y_validation, prediction3))
+print(classification_report(Y_validation, prediction3))
